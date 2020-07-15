32398688f7940bb4aec237801941b4b511330ee8
Albin Groen <albin.groen@gmail.com>
2020-07-15 11:45:24 +0200

    Add component documentation

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import 'basikit/dist/index.css'
+import { Container } from 'basikit'
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Container>
     <App />
-  </React.StrictMode>,
+  </Container>,
   document.getElementById('root')
-);
+)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
